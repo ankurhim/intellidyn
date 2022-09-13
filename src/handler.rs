9f35534614af9abbcf1ad15156506b1ca1211c7a
root <root@DESKTOP-NUK9MF0.localdomain>
2022-09-13 18:19:09 +0530

    upgraded the handler

--- src/handler.rs
+++ src/handler.rs
@@ -2,10 +2,11 @@ use lambda_http::{Body, Error, Request, IntoResponse, RequestExt, Response};
 use crate::db_client::DynamodbClient;
 
 pub async fn handler(event: Request) -> Result<Response<Body>, Error> {
-    let client = DynamodbClient::init().await;
+    let client = DynamodbClient::init().await?;
 
-    let resp = match &client {
-        Ok(_) => format!("Client connected!"),
+    let resp = match &client.check_tables("users").await {
+        Ok(true) => "Table Exists",
+        Ok(false) => "Table does not exist. Create one first.",
         Err(e) => format!("Error {:?}", e)
     };
 
