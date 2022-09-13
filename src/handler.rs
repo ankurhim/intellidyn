9a312793b768048496c3015e69a840e04055d536
root <root@DESKTOP-NUK9MF0.localdomain>
2022-09-13 12:04:55 +0530

    modules created

---
+++ src/handler.rs
@@ -0,0 +1,23 @@
+use lambda_http::{Body, Error, Request, IntoResponse, RequestExt, Response};
+use crate::db_client::DynamodbClient;
+
+pub async fn handler(event: Request) -> Result<Response<Body>, Error> {
+    let client = DynamodbClient::init().await;
+
+    let resp = match &client {
+        Ok(_) => format!("Client connected!"),
+        Err(e) => format!("Error {:?}", e)
+    };
+
+    Ok(match event.query_string_parameters().first("first_name") {
+        Some(v) => {
+            format!("{}: Client invoked by {:#?}. {:?}", event.lambda_context().request_id, v, resp)
+            .into_response()
+            .await
+        },
+        _ => Response::builder()
+        .status(400)
+        .body("Empty first name".into())
+        .expect("failed to render response"),
+    })
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 mod db_client;
+mod handler;
 
-use lambda_http::{service_fn, Body, Error, IntoResponse, Request, RequestExt, Response, tower::ServiceBuilder};
+use lambda_http::{service_fn, Error, tower::ServiceBuilder};
 use tower_http::cors::{ CorsLayer, Any};
-use crate::db_client::DynamodbClient;
+use crate::handler::handler;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -15,24 +16,9 @@ async fn main() -> Result<(), Error> {
     .allow_methods(Any)
     .allow_origin(Any);
 
-    let handler_func_closure = move |event: Request| async move {
-        Result::<Response<Body>, Error>::Ok(match event.query_string_parameters().first("first_name") {
-            Some(first_name) => {
-                DynamodbClient::init().await?;
-                format!("{}: Client invoked by {}.", event.lambda_context().request_id, first_name)
-                .into_response()
-                .await
-            },
-            _ => Response::builder()
-            .status(400)
-            .body("Empty first name".into())
-            .expect("failed to render response"),
-        })
-    };
-
     let service = ServiceBuilder::new()
     .layer(cors_layer)
-    .service(service_fn(handler_func_closure));
+    .service(service_fn(handler));
 
     lambda_http::run(service).await?;
     Ok(())
