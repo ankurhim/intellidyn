320f899a68e7e117938892fac827b3fb79956d2a
Ankur Himanshu <ankurhimanshu1986@gmail.com>
2023-05-05 16:38:00 +0530

    Added bom update

--- src/routes/bill_of_material/mod.rs
+++ src/routes/bill_of_material/mod.rs
@@ -1,15 +1,17 @@
 pub mod bill_of_material_model;
 pub mod create_bom;
 pub mod find_bom;
+pub mod update_bom;
 
 use axum::{
     Router,
-    routing::{post, get},
+    routing::{post, get, put},
 };
 
 use self::{
     create_bom::CreateBillOfMaterialRequest,
     find_bom::FindBillOfMaterialRequest,
+    update_bom::UpdateBillOfMaterialRequest,
 };
 
 pub async fn create_bom_routes() -> Router {
@@ -19,4 +21,5 @@ pub async fn create_bom_routes() -> Router {
     .route("/create_new_bom", post(CreateBillOfMaterialRequest::create_bom))
     .route("/find_table", get(FindBillOfMaterialRequest::find_bom_table))
     .route("/find_all_boms", get(FindBillOfMaterialRequest::find_all_boms))
+    .route("/update_bom", put(UpdateBillOfMaterialRequest::update_bom))
 }
